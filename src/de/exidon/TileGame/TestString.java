d39011433b16b341ea3d586160b0653866ec9506
Alexander <Alexander@192.168.0.14>
2017-09-07 17:12:23 +0200

    <Alexander@192.168.0.14>

---
+++ src/de/exidon/TileGame/TestString.java
@@ -0,0 +1,15 @@
+package de.exidon.TileGame;
+
+public class TestString {
+	private final String string="test";
+	public TestString()
+	{
+		
+	}
+	
+	public String getString()
+	{
+		return string;
+	}
+
+}
