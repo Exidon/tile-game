fbd0f06ce818dbf84757946976d1ae13ba5df4a0
Exidon <Exidon>
2017-09-07 17:28:32 +0200

    Window and GameLauncher
    Signed-off-by: Alexander <Alexander@192.168.0.14>

--- src/de/exidon/TileGame/GameLauncher.java
+++ src/de/exidon/TileGame/GameLauncher.java
@@ -1,10 +1,11 @@
 package de.exidon.TileGame;
 
 public class GameLauncher {
-	public static void main(String[] args) {
-	//	new window();
-	}
-	public void window(){
-		Display();
-	}
+    public GameLauncher(){
+        new Window();
+    }
+
+    public static void main(String[] args) {
+        new GameLauncher();
+    }
 }
---
+++ src/de/exidon/TileGame/Window.java
@@ -0,0 +1,16 @@
+package de.exidon.TileGame;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+
+    public Window(){
+        setTitle("Tile Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setContentPane(new GamePanel(1280, 720));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
